06df282f886e2eab491397d49c9dd11b2537ef39
pranshubarar1 <pranshubarar1851996@gmail.com>
2024-01-22 18:37:56 +0530

    Demo Class Calculator removed from tests

--- src/test/java/com/example/online_movie_ticketing_application/BookMyShowApplicationTests.java
+++ src/test/java/com/example/online_movie_ticketing_application/BookMyShowApplicationTests.java
@@ -6,44 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-//@SpringBootTest
+@SpringBootTest
 class BookMyShowApplicationTests {
-
-	private Calculator c = new Calculator();
-
 	@Test
 	@Disabled
 	void contextLoads() {
 	}
-
-	@Test
-	@Disabled
-	void testSum(){
-
-		//expected
-		int expectedResult = 17;
-
-		//actual
-		int actualResult = c.doSum(12,3,2);
-
-		assertThat(actualResult).isEqualTo(expectedResult);
-	}
-
-	@Test
-	void testProduct(){
-		//expectedResult
-		int expectedResult = 6;
-
-		//actual
-		int actualResult = c.doMult(3,2);
-
-		assertThat(actualResult).isEqualTo(expectedResult);
-	}
-
-	@Test
-	void testCompareNums(){
-		boolean actualResult = c.compareTwoNums(3,3);
-		assertThat(actualResult).isTrue();
-	}
-
 }
